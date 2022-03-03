47ce07f0fd3884daf4d0bc19ced9852b575ffd43
BufferC <1226098687@qq.com>
2022-03-03 14:50:38 +0800

    MyBatis-05|06|07|08
    properties、别名、日志

---
+++ 02-MyBatis-06-Alias/src/main/java/com/fc/test/AliasTest.java
@@ -0,0 +1,64 @@
+package com.fc.test;
+
+import com.fc.dao.StudentDao;
+import com.fc.entity.Student;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+
+public class AliasTest {
+    @Test
+    public void testFindById() {
+        try {
+            InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+
+            SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(inputStream);
+
+            SqlSession session = factory.openSession();
+
+            StudentDao studentDao = session.getMapper(StudentDao.class);
+
+            Student student = studentDao.findById(1);
+
+            System.out.println(student);
+
+            session.commit();
+
+            session.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testFindAll() {
+        try {
+            InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+
+            SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(inputStream);
+
+            SqlSession session = factory.openSession();
+
+            StudentDao studentDao = session.getMapper(StudentDao.class);
+
+            List<Student> students = studentDao.findAll();
+
+            for (Student student : students) {
+                System.out.println(student);
+            }
+
+            session.commit();
+
+            session.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
