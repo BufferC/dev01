d2fbfe0e76aed98ec8987173714e0749283a698a
Buffer Chang <69187455+BufferC@users.noreply.github.com>
2022-03-10 15:38:09 +0800

    Delete 03-Spring-00-ThirdLayerApplication-01-Web directory
    删除多余的项目目录

--- 03-Spring-00-ThirdLayerApplication-01-Web/src/main/java/com/fc/dao/UserDao.java
+++
@@ -1,5 +0,0 @@
-package com.fc.dao;
-
-public interface UserDao {
-    void useJDBC();
-}
--- 03-Spring-00-ThirdLayerApplication-01-Web/src/main/java/com/fc/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.fc.service;
-
-import com.fc.dao.UserDao;
-
-public interface UserService {
-    void useJDBC();
-
-    void setUserDao(UserDao userDao);
-}
