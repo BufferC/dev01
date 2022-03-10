fda406c9ea8d8b73b44b11fe46aa535a97591884
BufferC <1226098687@qq.com>
2022-03-10 16:01:54 +0800

    Spring-00
    重命名

--- 03-Spring-00-ThirdLayerApplication/src/main/java/com/fc/controller/StudentController.java
+++ 03-Spring-00-ThirdLayerApplication/src/main/java/com/fc/controller/StudentController.java
@@ -1,14 +1,12 @@
 package com.fc.controller;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 public class StudentController extends HttpServlet {
     @Override
-    public void init() throws ServletException {
+    public void init() {
         String username = getInitParameter("username");
         String password = getInitParameter("password");
 
@@ -16,12 +14,12 @@ public class StudentController extends HttpServlet {
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
 
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
         doGet(req, resp);
     }
 }
